db89ef61c51ddea8d6ac35d1f28e1b0524b67de0
Gundurao09 <kgundurao09@gmail.com>
2026-02-13 10:30:45 +0530

    Automated commit via n8n

---
+++ day_2_second_largest_element.py
@@ -0,0 +1,9 @@
+"""
+Docstring for day_2_second_largest_element
+
+Problem Statement: Find The Second Largest Element in an Array
+
+Input:  [5, 2, 9, 1, 7]
+Output: The Second Largest Element in an Array is : 7
+
+"""
